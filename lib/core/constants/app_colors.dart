8f84b94a684f2715c3646ac7092f17a5a63075d9
eomapps <apps@year2448.com>
2026-05-11 15:16:42 -0400

    chore: Add AppColors + AppStrings

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const brand = Color(0xFF534AB7);
+  static const buttonText = Color(0xFFEEEDFE);
+  static const colorDone = Color(0xFF1D9E75);
+  static const colorChoice1 = Color(0xFF378ADD);
+  static const colorChoice2 = Color(0xFFD85A30);
+  static const colorChoice3 = Color(0xFF1D9E75);
+  static const colorChoice4 = Color(0xFF7F77DD);
+  static const colorChoice5 = Color(0xFFD4537E);
+}
---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,15 @@
+class AppStrings {
+  static const String appTitle = 'HabitTrack';
+  static const String appTagLine = 'build better habits';
+  static const String todaysHabits = 'Today\'s Habits';
+  static const String notDone = 'NOT DONE';
+  static const String doneToday = 'DONE TODAY';
+  static const String habitName = 'Habit Name';
+  static const String colorChoice = 'Color';
+  static const String saveHabit = 'Save Habit';
+  static const String progress = 'Progress';
+  static const String today = 'Today';
+  static const String noHabitsYet = 'No habits yet';
+  static const String addPrompt =
+      'Tap the + button to add your first habit and start building your streak.';
+}
