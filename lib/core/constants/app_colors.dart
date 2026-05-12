0931fab3a72d31c7daa6ee1e965fdc83890d76b5
eomapps <apps@year2448.com>
2026-05-12 13:24:30 -0400

    chore: updated AppColors values

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const brand = Color(0xFF534AB7);
+  AppColors._();
+
+  static const purple = Color(0xFF534AB7);
+  static const purpleMid = Color(0xFF7F77DD);
+  static const purpleLight = Color(0xFFEEEDFE);
   static const buttonText = Color(0xFFEEEDFE);
-  static const colorDone = Color(0xFF1D9E75);
-  static const colorChoice1 = Color(0xFF378ADD);
-  static const colorChoice2 = Color(0xFFD85A30);
-  static const colorChoice3 = Color(0xFF1D9E75);
-  static const colorChoice4 = Color(0xFF7F77DD);
-  static const colorChoice5 = Color(0xFFD4537E);
-  static const mutedText = Color(0xFF7A789A);
+  static const green = Color(0xFF1D9E75);
+  static const blue = Color(0xFF378ADD);
+  static const orange = Color(0xFFD85A30);
+  static const pink = Color(0xFFD4537E);
+  static const bg = Color(0xFFF0EFF8);
+  static const card = Colors.white;
+  static const card2 = Color(0xFFF7F6FC);
+  static const text = Color(0xFF18172B);
+  static final border = Color(0xFF534AB7).withOpacity(0.10);
+  static final label = Color(0xFF534AB7).withOpacity(0.35);
+  static const muted = Color(0xFF7A789A);
 }
