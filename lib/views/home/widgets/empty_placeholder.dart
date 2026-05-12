57d9a51e138be080ab1f3c387b32b5f78b42e3c1
eomapps <apps@year2448.com>
2026-05-11 23:10:42 -0400

    feat: Add EmptyPlaceholder, with home_icon.svg, pubspec.yaml dependency

--- lib/views/home/widgets/empty_placeholder.dart
+++ lib/views/home/widgets/empty_placeholder.dart
@@ -1,1 +1,28 @@
-// TODO implement
+import 'package:eomappshabit_track/core/constants/app_strings.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class EmptyPlaceholder extends StatelessWidget {
+  const EmptyPlaceholder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SvgPicture.asset('assets/images/home_icon.svg'),
+        SizedBox(height: 10),
+        Text(
+          AppStrings.noHabitsYet,
+          style: Theme.of(context).textTheme.bodyLarge,
+          textAlign: TextAlign.center,
+        ),
+        SizedBox(height: 10),
+        Text(
+          AppStrings.addPrompt,
+          style: Theme.of(context).textTheme.bodyLarge,
+          textAlign: TextAlign.center,
+        ),
+      ],
+    );
+  }
+}
