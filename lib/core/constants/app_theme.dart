8a0410c8b7a8a1b6c486ec7bbbdfb887781582db
eomapps <apps@year2448.com>
2026-05-12 13:25:54 -0400

    chore: updated AppTheme values for TextTheme and added static constructor

--- lib/core/constants/app_theme.dart
+++ lib/core/constants/app_theme.dart
@@ -2,18 +2,20 @@ import 'package:eomappshabit_track/core/constants/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
+  AppTheme._();
+
   static ThemeData get light => ThemeData(
     useMaterial3: true,
     colorScheme: ColorScheme.light(
-      primary: AppColors.brand,
+      primary: AppColors.purple,
       onPrimary: AppColors.buttonText,
     ),
     textTheme: TextTheme(
       headlineLarge: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
       bodyLarge: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-      bodyMedium: TextStyle(fontSize: 14, color: AppColors.mutedText),
+      bodyMedium: TextStyle(fontSize: 14, color: AppColors.muted),
       bodySmall: TextStyle(fontSize: 10),
     ),
-    buttonTheme: ButtonThemeData(buttonColor: AppColors.brand),
+    buttonTheme: ButtonThemeData(buttonColor: AppColors.purple),
   );
 }
