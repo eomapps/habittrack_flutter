3d6244663810a481e86100c9fc66642b3927864e
eomapps <apps@year2448.com>
2026-05-12 14:40:35 -0400

    style: add Color as type for formatting consistency with other classes in /constants

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -3,19 +3,19 @@ import 'package:flutter/material.dart';
 class AppColors {
   AppColors._();
 
-  static const purple = Color(0xFF534AB7);
-  static const purpleMid = Color(0xFF7F77DD);
-  static const purpleLight = Color(0xFFEEEDFE);
-  static const buttonText = Color(0xFFEEEDFE);
-  static const green = Color(0xFF1D9E75);
-  static const blue = Color(0xFF378ADD);
-  static const orange = Color(0xFFD85A30);
-  static const pink = Color(0xFFD4537E);
-  static const bg = Color(0xFFF0EFF8);
-  static const card = Colors.white;
-  static const card2 = Color(0xFFF7F6FC);
-  static const text = Color(0xFF18172B);
-  static final border = Color(0xFF534AB7).withOpacity(0.10);
-  static final label = Color(0xFF534AB7).withOpacity(0.35);
-  static const muted = Color(0xFF7A789A);
+  static const Color purple = Color(0xFF534AB7);
+  static const Color purpleMid = Color(0xFF7F77DD);
+  static const Color purpleLight = Color(0xFFEEEDFE);
+  static const Color buttonText = Color(0xFFEEEDFE);
+  static const Color green = Color(0xFF1D9E75);
+  static const Color blue = Color(0xFF378ADD);
+  static const Color orange = Color(0xFFD85A30);
+  static const Color pink = Color(0xFFD4537E);
+  static const Color bg = Color(0xFFF0EFF8);
+  static const Color card = Colors.white;
+  static const Color card2 = Color(0xFFF7F6FC);
+  static const Color text = Color(0xFF18172B);
+  static final Color border = Color(0xFF534AB7).withOpacity(0.10);
+  static final Color label = Color(0xFF534AB7).withOpacity(0.35);
+  static const Color muted = Color(0xFF7A789A);
 }
