474b28700f333a4ce07da9686ede5703855b8083
eomapps <apps@year2448.com>
2026-05-13 16:00:34 -0400

    feat: add FAB button to AppBar actions

--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -1,4 +1,7 @@
+import 'package:eomappshabit_track/core/constants/app_colors.dart';
+import 'package:eomappshabit_track/core/constants/app_dimens.dart';
 import 'package:eomappshabit_track/core/constants/app_strings.dart';
+import 'package:eomappshabit_track/core/constants/app_text_styles.dart';
 import 'package:eomappshabit_track/views/home/widgets/empty_placeholder.dart';
 import 'package:flutter/material.dart';
 
@@ -13,7 +16,30 @@ class HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(AppStrings.today), centerTitle: false),
+      appBar: AppBar(
+        title: Text(AppStrings.today, style: AppTextStyles.appBarTitle),
+        centerTitle: false,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 12),
+            child: GestureDetector(
+              onTap: () {
+                // TODO implement to open the addeditbottomsheet
+              },
+              child: Container(
+                height: AppDimens.fabSize,
+                width: AppDimens.fabSize,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: AppColors.purple,
+                  boxShadow: [AppDimens.fabShadow],
+                ),
+                child: Icon(Icons.add, color: Colors.white, size: 20),
+              ),
+            ),
+          ),
+        ],
+      ),
       body: Column(children: [Expanded(child: EmptyPlaceholder())]),
     );
   }
