6a4b319ff11baf13da654bbe199e3b9f67bde9f5
eomapps <apps@year2448.com>
2026-05-13 13:44:17 -0400

    chore: update AppColors with emptyStateIconBg

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -18,4 +18,5 @@ class AppColors {
   static final Color border = Color(0xFF534AB7).withOpacity(0.10);
   static final Color label = Color(0xFF534AB7).withOpacity(0.35);
   static const Color muted = Color(0xFF7A789A);
+  static final Color emptyStateIconBg = purpleMid.withOpacity(0.20);
 }
