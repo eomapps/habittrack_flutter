a5502a4e6211e0ef084131fdbf45805c922683ed
eomapps <apps@year2448.com>
2026-05-13 13:48:09 -0400

    feat: implement HomeScreen scaffold with EmptyPlaceholder

--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:eomappshabit_track/core/constants/app_strings.dart';
+import 'package:eomappshabit_track/views/home/widgets/empty_placeholder.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -12,13 +13,8 @@ class HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          AppStrings.today,
-          style: Theme.of(context).textTheme.headlineLarge,
-        ),
-        centerTitle: false,
-      ),
+      appBar: AppBar(title: Text(AppStrings.today), centerTitle: false),
+      body: Column(children: [Expanded(child: EmptyPlaceholder())]),
     );
   }
 }
