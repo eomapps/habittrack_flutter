f4517bc146b23fadbaab7c88fed02d2ea19df8e3
eomapps <apps@year2448.com>
2026-05-14 12:43:48 -0400

    feat: filter habits into done/not-done sections on home screen; show EmptyPlaceholder when empty

--- lib/views/home/widgets/today.dart
+++ lib/views/home/widgets/today.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:habittrack/core/constants/app_dimens.dart';
+import 'package:habittrack/core/constants/app_strings.dart';
+import 'package:habittrack/core/constants/app_text_styles.dart';
 import 'package:habittrack/core/utils/ht_utils.dart';
+import 'package:habittrack/data/models/habit.dart';
+import 'package:habittrack/viewmodels/habit_viewmodel.dart';
 
 import 'package:habittrack/views/home/widgets/empty_placeholder.dart';
+import 'package:habittrack/views/home/widgets/habit_card.dart';
+import 'package:provider/provider.dart';
 
 class TodayScreen extends StatefulWidget {
   const TodayScreen({super.key});
@@ -18,9 +25,48 @@ class _TodayScreenState extends State<TodayScreen> {
       children: [
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Text(HTUtils.getFormattedDate()),
+          child: Text(HTUtils.getFormattedDate(), style: AppTextStyles.dateRow),
         ),
-        Expanded(child: Center(child: EmptyPlaceholder())),
+        Consumer<HabitViewModel>(
+          builder: (context, value, child) {
+            if (value.habits.isEmpty) {
+              return const Expanded(child: Center(child: EmptyPlaceholder()));
+            }
+
+            final today = DateTime.now().toIso8601String().substring(0, 10);
+            final notDone = value.habits
+                .where((h) => h.lastCheckedDate != today)
+                .toList();
+            final done = value.habits
+                .where((h) => h.lastCheckedDate == today)
+                .toList();
+
+            return Column(
+              children: [
+                if (notDone.isNotEmpty) ...[
+                  Padding(
+                    padding: AppDimens.paddingSectionLabel,
+                    child: Text(
+                      AppStrings.notDone,
+                      style: AppTextStyles.sectionLabel,
+                    ),
+                  ),
+                  ...notDone.map((habit) => HabitCard(habit: habit)),
+                ],
+                if (done.isNotEmpty) ...[
+                  Padding(
+                    padding: AppDimens.paddingSectionLabel,
+                    child: Text(
+                      AppStrings.doneToday,
+                      style: AppTextStyles.sectionLabel,
+                    ),
+                  ),
+                  ...done.map((habit) => HabitCard(habit: habit)),
+                ],
+              ],
+            );
+          },
+        ),
       ],
     );
   }
