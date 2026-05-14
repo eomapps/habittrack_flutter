b52cdb1724c833da8b1fc38a0223c441f61c08a6
eomapps <apps@year2448.com>
2026-05-14 12:38:42 -0400

    feat: implement HabitCard widget with done/undone states

--- lib/views/home/widgets/habit_card.dart
+++ lib/views/home/widgets/habit_card.dart
@@ -1,1 +1,75 @@
-// TODO implement
+import 'package:flutter/material.dart';
+import 'package:habittrack/core/constants/app_colors.dart';
+import 'package:habittrack/core/constants/app_dimens.dart';
+import 'package:habittrack/core/constants/app_text_styles.dart';
+import 'package:habittrack/data/models/habit.dart';
+
+class HabitCard extends StatefulWidget {
+  final Habit habit;
+  final bool isDone;
+
+  const HabitCard({super.key, required this.habit, this.isDone = false});
+
+  @override
+  State<HabitCard> createState() => _HabitCardState();
+}
+
+class _HabitCardState extends State<HabitCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: AppColors.cardBg,
+        borderRadius: BorderRadius.circular(AppDimens.radiusCard),
+        border: Border.all(color: AppColors.border, width: 1),
+      ),
+      margin: AppDimens.paddingHabitCardMargin,
+      padding: AppDimens.paddingHabitCard,
+      child: Row(
+        children: [
+          Container(
+            height: AppDimens.colorDotSize,
+            width: AppDimens.colorDotSize,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Color(int.parse('0xFF${widget.habit.colorHex}')),
+            ),
+          ),
+          const SizedBox(width: 10),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(widget.habit.title, style: AppTextStyles.habitName),
+                const SizedBox(width: 2),
+                Text(
+                  '${widget.habit.streakCount} day streak',
+                  style: AppTextStyles.habitStreak,
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(width: 10),
+          Container(
+            height: AppDimens.checkCircleSize,
+            width: AppDimens.checkCircleSize,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: widget.isDone
+                  ? Color(int.parse('0xFF${widget.habit.colorHex}'))
+                  : AppColors.card,
+              boxShadow: widget.isDone ? [AppDimens.checkDoneShadow] : null,
+              border: widget.isDone
+                  ? null
+                  : Border.all(color: AppColors.border, width: 1.5),
+            ),
+            child: widget.isDone
+                ? Icon(Icons.check, color: Colors.white)
+                : null,
+          ),
+        ],
+      ),
+    );
+  }
+}
