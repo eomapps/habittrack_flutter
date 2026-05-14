dea7b599b32ce78378380218bca1eaadf036c8c1
eomapps <apps@year2448.com>
2026-05-14 00:13:36 -0400

    feat: implement BottomNavigationBar in HomeScreen

--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -1,23 +1,31 @@
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:habittrack/core/constants/app_colors.dart';
 import 'package:habittrack/core/constants/app_dimens.dart';
 import 'package:habittrack/core/constants/app_strings.dart';
 import 'package:habittrack/core/constants/app_text_styles.dart';
-import 'package:habittrack/views/home/widgets/empty_placeholder.dart';
 import 'package:flutter/material.dart';
+import 'package:habittrack/views/home/widgets/progress.dart';
+import 'package:habittrack/views/home/widgets/today.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  State<StatefulWidget> createState() => HomeScreenState();
+  State<StatefulWidget> createState() => _HomeScreenState();
 }
 
-class HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+  final List<Widget> _tabs = [TodayScreen(), ProgressScreen()];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(AppStrings.today, style: AppTextStyles.appBarTitle),
+        title: Text(
+          _currentIndex == 0 ? AppStrings.today : AppStrings.progress,
+          style: AppTextStyles.appBarTitle,
+        ),
         centerTitle: false,
         actions: [
           Padding(
@@ -26,6 +34,7 @@ class HomeScreenState extends State<HomeScreen> {
               onTap: () {
                 // TODO implement to open the addeditbottomsheet
               },
+              behavior: HitTestBehavior.opaque,
               child: Container(
                 height: AppDimens.fabSize,
                 width: AppDimens.fabSize,
@@ -40,7 +49,26 @@ class HomeScreenState extends State<HomeScreen> {
           ),
         ],
       ),
-      body: Column(children: [Expanded(child: EmptyPlaceholder())]),
+      body: IndexedStack(index: _currentIndex, children: _tabs),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        selectedLabelStyle: AppTextStyles.navLabelActive,
+        unselectedLabelStyle: AppTextStyles.navLabel,
+        backgroundColor: AppColors.card,
+        selectedItemColor: AppColors.purple,
+        unselectedItemColor: AppColors.muted,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.today_rounded, size: 18),
+            label: AppStrings.today,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.view_timeline_rounded, size: 18),
+            label: AppStrings.progress,
+          ),
+        ],
+      ),
     );
   }
 }
