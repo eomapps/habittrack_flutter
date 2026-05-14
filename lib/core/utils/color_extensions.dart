b4a5a280347285e164e7f986937c0e64c265b658
eomapps <apps@year2448.com>
2026-05-14 15:05:02 -0400

    add ColorX extension and colorFromHex helper

---
+++ lib/core/utils/color_extensions.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+extension ColorX on Color {
+  String toHex() =>
+      '#${toARGB32().toRadixString(16).substring(2).toUpperCase()}';
+}
+
+Color colorFromHex(String hex) {
+  final cleaned = hex
+      .replaceFirst('#', '')
+      .replaceFirst('0xFF', '')
+      .replaceFirst('0xff', '');
+  return Color(int.parse('FF$cleaned', radix: 16));
+}
