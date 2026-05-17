cc2eb878ac1c17332ebcc19bfe6a7055ce906f9a
eomapps <apps@year2448.com>
2026-05-17 18:40:13 -0400

    feat: add ProgressPlaceholder

---
+++ lib/views/home/widgets/progress_placeholder.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:habittrack/core/constants/app_colors.dart';
+import 'package:habittrack/core/constants/app_dimens.dart';
+import 'package:habittrack/core/constants/app_strings.dart';
+import 'package:habittrack/core/constants/app_text_styles.dart';
+
+class ProgressPlaceholder extends StatelessWidget {
+  Widget build(BuildContext context) {
+    final spacing = MediaQuery.of(context).size.height * 0.02;
+
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            height: 64,
+            width: 64,
+            decoration: BoxDecoration(
+              color: AppColors.emptyStateIconBg,
+              borderRadius: BorderRadius.circular(
+                AppDimens.radiusEmptyStateIconBox,
+              ),
+            ),
+            child: Center(
+              child: Icon(
+                Icons.bar_chart_rounded,
+                color: AppColors.purpleLight,
+                size: 48,
+              ),
+            ),
+          ),
+          SizedBox(height: spacing),
+          Text(
+            AppStrings.noProgressYet,
+            style: AppTextStyles.emptyStateTitle,
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: spacing),
+          Text(
+            AppStrings.addProgress,
+            style: AppTextStyles.emptyStateSub,
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
