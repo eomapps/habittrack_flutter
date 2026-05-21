4d65ceadc42cda96f9128a67e7ba75ffdab00560
eomapps <apps@year2448.com>
2026-05-20 22:30:16 -0400

    fix: update date on resume and at midnight via one-shot timer

--- lib/views/home/widgets/today.dart
+++ lib/views/home/widgets/today.dart
@@ -1,28 +1,79 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:habittrack/core/constants/app_dimens.dart';
 import 'package:habittrack/core/constants/app_strings.dart';
 import 'package:habittrack/core/constants/app_text_styles.dart';
 import 'package:habittrack/core/utils/ht_utils.dart';
 import 'package:habittrack/viewmodels/habit_viewmodel.dart';
-
 import 'package:habittrack/views/home/widgets/today_placeholder.dart';
 import 'package:habittrack/views/home/widgets/habit_card.dart';
 import 'package:provider/provider.dart';
 
 class TodayScreen extends StatefulWidget {
-  const TodayScreen({super.key});
+  final DateTime? dateTime;
+  const TodayScreen({super.key, this.dateTime});
 
   @override
   State<TodayScreen> createState() => _TodayScreenState();
 }
 
-class _TodayScreenState extends State<TodayScreen> {
+class _TodayScreenState extends State<TodayScreen> with WidgetsBindingObserver {
+  late String dateToday;
+  Timer? timer;
+
   @override
   void initState() {
     super.initState();
+    WidgetsBinding.instance.addObserver(this);
     WidgetsBinding.instance.addPostFrameCallback((_) {
       context.read<HabitViewModel>().getAllHabits();
     });
+    dateToday = HTUtils.getFormattedDate(widget.dateTime);
+    _initTimer();
+  }
+
+  void _initTimer() {
+    final now = DateTime.now().add(Duration(days: 1));
+    final nextMidnight = DateTime(now.year, now.month, now.day);
+    final durationToUse = nextMidnight.difference(DateTime.now());
+    if (durationToUse.inMinutes <= 0) {
+      if (HTUtils.getFormattedDate(DateTime.now()) != dateToday) {
+        setState(() {
+          dateToday = HTUtils.getFormattedDate(DateTime.now());
+        });
+      }
+      timer = Timer(Duration(hours: 24), () => onEnd());
+    } else {
+      timer = Timer(Duration(minutes: durationToUse.inMinutes), () => onEnd());
+    }
+  }
+
+  void onEnd() {
+    if (mounted) {
+      if (HTUtils.getFormattedDate(DateTime.now()) != dateToday) {
+        setState(() {
+          dateToday = HTUtils.getFormattedDate(DateTime.now());
+        });
+      }
+      _initTimer();
+    }
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      setState(() {
+        dateToday = HTUtils.getFormattedDate(widget.dateTime);
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    timer?.cancel();
+    super.dispose();
   }
 
   @override
@@ -32,14 +83,17 @@ class _TodayScreenState extends State<TodayScreen> {
       children: [
         Padding(
           padding: AppDimens.paddingDateRow,
-          child: Text(HTUtils.getFormattedDate(), style: AppTextStyles.dateRow),
+          child: Text(
+            dateToday,
+            style: AppTextStyles.dateRow,
+            key: const Key('date-today-text'),
+          ),
         ),
         Consumer<HabitViewModel>(
           builder: (context, value, child) {
             if (value.habits.isEmpty) {
               return const Expanded(child: Center(child: TodayPlaceholder()));
             }
-
             final today = DateTime.now().toIso8601String().substring(0, 10);
             final notDone = value.habits
                 .where((h) => h.lastCheckedDate != today)
